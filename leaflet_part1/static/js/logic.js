f8432ca2eeff82c1b4fe2c39c4778e34f66f0e8a
xc1614 <124749040+xc1614@users.noreply.github.com>
2023-05-31 22:38:26 -0400

    edit

--- leaflet_part1/static/js/logic.js
+++ leaflet_part1/static/js/logic.js
@@ -1,67 +1,67 @@
 // Creating the map object
 let myMap = L.map("map", {
-    center: [37.7749, -122.4194],
-    zoom: 6
+  center: [37.7749, -122.4194],
+  zoom: 6
+});
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(url).then(function(response) {
+  // Process the earthquake data
+  let earthquakes = response.features;
+
+  // Loop through the earthquakes data and add markers with size and color based on magnitude
+  earthquakes.forEach(function(earthquake) {
+    let coords = earthquake.geometry.coordinates;
+    let magnitude = earthquake.properties.mag;
+
+    // Calculate marker size based on magnitude
+    let markerSize = magnitude * 5;
+
+    // Calculate marker color based on magnitude
+    let markerColor = getColor(magnitude);
+
+    // Create a circle marker with size and color
+    L.circleMarker([coords[1], coords[0]], {
+      radius: markerSize,
+      fillColor: markerColor,
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 1,
+    }).addTo(myMap)
+      .bindPopup(`<strong>Place:</strong> ${earthquake.properties.place}<br>
+                  <strong>Magnitude:</strong> ${magnitude}<br>`);
   });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-  let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-  
-  d3.json(url).then(function(response) {
-    // Process the earthquake data
-    let earthquakes = response.features;
-  
-    // Loop through the earthquakes data and add markers with size and color based on magnitude
-    earthquakes.forEach(function(earthquake) {
-      let coords = earthquake.geometry.coordinates;
-      let magnitude = earthquake.properties.mag;
-  
-      // Calculate marker size based on magnitude
-      let markerSize = magnitude * 5;
-  
-      // Calculate marker color based on magnitude
-      let markerColor = getColor(magnitude);
-  
-      // Create a circle marker with size and color
-      L.circleMarker([coords[1], coords[0]], {
-        radius: markerSize,
-        fillColor: markerColor,
-        color: "#000000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 1,
-      }).addTo(myMap)
-        .bindPopup(`<strong>Place:</strong> ${earthquake.properties.place}<br>
-                    <strong>Magnitude:</strong> ${magnitude}<br>`);
-    });
-  
-    // Create a legend for the map
-    let legend = L.control({ position: "bottomright" });
-  
-    legend.onAdd = function() {
-      let div = L.DomUtil.create("div", "info legend");
-      let grades = [0, 1, 2, 3, 4, 5];
-      let colors = ["#00ff00", "#66ff00", "#ccff00", "#ffff00", "#ffcc00", "#ff0000"];
-      let lables = [];
-  
-      div.innerHTML += "<strong>Magnitude</strong><br>";
-  
-      for (let i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-          '<i style="background:' + colors[i] + '"></i> ' +
-          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-      }
-  
-      return div;
-    };
-  
-    legend.addTo(myMap);
-  });
-  
+
+  // Create a legend for the map
+  let legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    let grades = [0, 1, 2, 3, 4, 5];
+    let colors = ["#00ff00", "#66ff00", "#ccff00", "#ffff00", "#ffcc00", "#ff0000"];
+    let labels = [];
+
+    // div.innerHTML += "<strong>Magnitude</strong><br>";
+
+    for (let i = 0; i < grades.length; i++) {
+      labels.push('<ul style="background-color:' + colors[i] + '"><span>' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '' : '+') + '</span></ul>');
+    }
+
+    // Add each label list item to the div under the <ul> tag
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+
+    return div;
+  };
+
+  legend.addTo(myMap);
+
   // Define the getColor function to calculate marker color based on magnitude
   function getColor(magnitude) {
     switch (true) {
@@ -79,4 +79,5 @@ let myMap = L.map("map", {
         return "#ff0000";
     }
   }
+});
 
