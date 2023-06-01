b6d32356a28e78456f4bf308ad4e9c53f2bec6b2
xc1614 <124749040+xc1614@users.noreply.github.com>
2023-05-31 21:48:51 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # leaflet-challenge
+
+Your first task is to visualize an earthquake dataset. Complete the following steps:
+
+Get your dataset. To do so, follow these steps:
+
+The USGS provides earthquake data in a number of different formats, updated every 5 minutes. Visit the USGS GeoJSON FeedLinks to an external site. page and choose a dataset to visualize. The following image is an example screenshot of what appears when you visit this link:
+3-Data
+
+When you click a dataset (such as "All Earthquakes from the Past 7 Days"), you will be given a JSON representation of that data. Use the URL of this JSON to pull in the data for the visualization. The following image is a sampling of earthquake data in JSON format:
+4-JSON
+
+Import and visualize the data by doing the following:
+
+Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+
+Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+
+Hint: The depth of the earth can be found as the third coordinate for each earthquake.
+
+Include popups that provide additional information about the earthquake when its associated marker is clicked.
+
+Create a legend that will provide context for your map data.
+
+Your visualization should look something like the preceding map.
---
+++ leaflet_part1/static/js/logic.js
@@ -0,0 +1,82 @@
+// Creating the map object
+let myMap = L.map("map", {
+    center: [37.7749, -122.4194],
+    zoom: 6
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
+  let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+  
+  d3.json(url).then(function(response) {
+    // Process the earthquake data
+    let earthquakes = response.features;
+  
+    // Loop through the earthquakes data and add markers with size and color based on magnitude
+    earthquakes.forEach(function(earthquake) {
+      let coords = earthquake.geometry.coordinates;
+      let magnitude = earthquake.properties.mag;
+  
+      // Calculate marker size based on magnitude
+      let markerSize = magnitude * 5;
+  
+      // Calculate marker color based on magnitude
+      let markerColor = getColor(magnitude);
+  
+      // Create a circle marker with size and color
+      L.circleMarker([coords[1], coords[0]], {
+        radius: markerSize,
+        fillColor: markerColor,
+        color: "#000000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 1,
+      }).addTo(myMap)
+        .bindPopup(`<strong>Place:</strong> ${earthquake.properties.place}<br>
+                    <strong>Magnitude:</strong> ${magnitude}<br>`);
+    });
+  
+    // Create a legend for the map
+    let legend = L.control({ position: "bottomright" });
+  
+    legend.onAdd = function() {
+      let div = L.DomUtil.create("div", "info legend");
+      let grades = [0, 1, 2, 3, 4, 5];
+      let colors = ["#00ff00", "#66ff00", "#ccff00", "#ffff00", "#ffcc00", "#ff0000"];
+      let lables = [];
+  
+      div.innerHTML += "<strong>Magnitude</strong><br>";
+  
+      for (let i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' + colors[i] + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+  
+      return div;
+    };
+  
+    legend.addTo(myMap);
+  });
+  
+  // Define the getColor function to calculate marker color based on magnitude
+  function getColor(magnitude) {
+    switch (true) {
+      case magnitude < 1:
+        return "#00ff00";
+      case magnitude < 2:
+        return "#66ff00";
+      case magnitude < 3:
+        return "#ccff00";
+      case magnitude < 4:
+        return "#ffff00";
+      case magnitude < 5:
+        return "#ffcc00";
+      default:
+        return "#ff0000";
+    }
+  }
+
